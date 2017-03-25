5046a45fb4a49d2ad95a3874e5976a31ead918df
billofbong <willjwright@mac.com>
2017-03-25 16:03:41 -0700

    Added Unity assets

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float playerSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		transform.Translate(movement * Time.deltaTime * playerSpeed);
+
+		if(Input.GetAxisRaw("Horizontal") == -1) {
+			transform.localScale = new Vector2(-1, 1);
+		}
+		if(Input.GetAxisRaw("Horizontal") == 1) {
+			transform.localScale = new Vector2(1, 1);
+		}
+	}
+}
